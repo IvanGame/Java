8b44775eaa2497a7c5b86b58d17d81f68128c26b
IvanGame <ivan5456007@gmail.com>
2023-07-14 21:46:49 +0700

    seminar 3

--- ex_1.java
+++ ex_1.java
@@ -1,21 +1,70 @@
 package Java.Java;
 
-public class ex_1 {
-    public static final String QUERY = "select * from students where ";
-    public static final String PARAMS = "{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"} ";
+import java.util.ArrayList;
+import java.util.List;
 
+public class ex_1 {
     public static void main(String[] args) {
-        String paramsNew = PARAMS.replace('{', ' ').replace('}', ' ');
-        String[] params = paramsNew.split(",");
-        StringBuilder stringBuilder = new StringBuilder(QUERY);
-        for (int i = 0; i < params.length; i++) {
-            String[] elements = params[i].replace('"', ' ').split(":");
-            if (!"null".equals(elements[1].trim())) {
-                stringBuilder.append(elements[0].trim()).append("=").append("'").append(elements[1].trim()).append("'");
-                if (i < params.length - 2)
-                    stringBuilder.append(" and ");
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(2);
+        numbers.add(5);
+        numbers.add(10);
+        numbers.add(7);
+        numbers.add(1);
+        numbers.add(8);
+
+        // Удаление четных чисел
+        List<Integer> oddNumbers = removeEvenNumbers(numbers);
+        System.out.println("Список после удаления четных чисел: " + oddNumbers);
+
+        // Поиск минимального значения
+        int min = findMinimum(oddNumbers);
+        System.out.println("Минимальное значение: " + min);
+
+        // Поиск максимального значения
+        int max = findMaximum(oddNumbers);
+        System.out.println("Максимальное значение: " + max);
+
+        // Поиск среднего значения
+        double average = findAverage(oddNumbers);
+        System.out.println("Среднее значение: " + average);
+    }
+
+    public static List<Integer> removeEvenNumbers(List<Integer> numbers) {
+        List<Integer> oddNumbers = new ArrayList<>();
+        for (int number : numbers) {
+            if (number % 2 != 0) {
+                oddNumbers.add(number);
             }
         }
-        System.out.println(stringBuilder);
+        return oddNumbers;
+    }
+
+    public static int findMinimum(List<Integer> numbers) {
+        int min = Integer.MAX_VALUE;
+        for (int number : numbers) {
+            if (number < min) {
+                min = number;
+            }
+        }
+        return min;
+    }
+
+    public static int findMaximum(List<Integer> numbers) {
+        int max = Integer.MIN_VALUE;
+        for (int number : numbers) {
+            if (number > max) {
+                max = number;
+            }
+        }
+        return max;
+    }
+
+    public static double findAverage(List<Integer> numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return (double) sum / numbers.size();
     }
 }
